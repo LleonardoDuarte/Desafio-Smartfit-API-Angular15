66472152e59de2aea135480bac24b10c9f79e918
LeonardoDuarte <leopd_00@hotmail.com>
2024-03-12 13:49:25 -0300

    feat: Logica para filtrar as academias e realizar as comparações para retorno correto das informaçoes selecionadas.

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from 'src/app/types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -8,26 +10,36 @@ import { GetUnitsService } from 'src/app/services/get-units.service';
   styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent {
-  public results = ['Um dia', 'Será seu'];
+  public results: Location[] = [];
+  filteredResults: Location[] = [];
 
   public formGroup!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private getUnitsService: GetUnitsService
+    private getUnitsService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService
   ) {}
 
   ngOnInit(): void {
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false,
+      showClosed: true,
     });
 
-    this.getUnitsService.getAllUnits().subscribe((res) => console.log(res));
+    this.getUnitsService.getAllUnits().subscribe((res) => {
+      this.results = res.locations;
+      this.filteredResults = res.locations;
+    });
   }
 
   public onSubmit() {
-    console.log(this.formGroup.value);
+    let { showClosed, hour } = this.formGroup.value;
+    this.filteredResults = this.filterUnitsService.filter(
+      this.results,
+      showClosed,
+      hour
+    );
   }
   public onClean() {
     this.formGroup.reset();
